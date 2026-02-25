d243e67e6999ecf11e5cadf224d02d6aad271bed
RONALD <RONALD10CEC@GMAIL.COM>
2026-02-24 23:24:08 -0400

    SEO MEJRADO

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="google-site-verification" content="EKwmoruhL1ItI2JjXMuTl9FAFpHUTzse6InjhjAHpw4" />
 
     {{-- ====== SEO Básico ====== --}}
     <title>{{ $seo['title'] }}</title>
